8c40b3ec177c148136c58924a84c3f2f0789e9bb
fenrisulfr <shijun.luo@leyoutech.com>
2021-01-27 19:03:37 +0800

    Test texture include.

--- opengl/shader/fragment.fs
+++ opengl/shader/fragment.fs
@@ -1,6 +1,16 @@
 #version 330 core
+
 out vec4 FragColor;
+in vec4 myColor;
+in vec2 myUv;
+
+uniform vec4 ourColor;
+
+uniform sampler2D ourTexture;
+
+
 void main()
 {
-    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    FragColor = texture(ourTexture, myUv) * myColor;
+
 }
